99e55ff56b132d0a9a975fb068bd13a36b4033c1
baseballproject1 <ryuhj0829@gmail.com>
2026-07-24 09:02:43 +0900

    Add KBO teams data to teams.js

---
+++ teams.js
@@ -0,0 +1,149 @@
+// ==========================
+// KBO 구단 데이터
+// teams.js
+// ==========================
+
+
+let teams = {
+
+
+SSG:{
+
+name:"SSG 랜더스",
+
+color:"#e60012",
+
+money:150,
+
+power:90
+
+},
+
+
+
+LG:{
+
+name:"LG 트윈스",
+
+color:"#c9003d",
+
+money:150,
+
+power:92
+
+},
+
+
+
+삼성:{
+
+name:"삼성 라이온즈",
+
+color:"#005bac",
+
+money:150,
+
+power:88
+
+},
+
+
+
+두산:{
+
+name:"두산 베어스",
+
+color:"#131230",
+
+money:150,
+
+power:87
+
+},
+
+
+
+KIA:{
+
+name:"KIA 타이거즈",
+
+color:"#ea0029",
+
+money:150,
+
+power:91
+
+},
+
+
+
+KT:{
+
+name:"KT 위즈",
+
+color:"#000000",
+
+money:150,
+
+power:89
+
+},
+
+
+
+롯데:{
+
+name:"롯데 자이언츠",
+
+color:"#041e42",
+
+money:150,
+
+power:85
+
+},
+
+
+
+한화:{
+
+name:"한화 이글스",
+
+color:"#ff6600",
+
+money:150,
+
+power:86
+
+},
+
+
+
+키움:{
+
+name:"키움 히어로즈",
+
+color:"#570514",
+
+money:150,
+
+power:84
+
+},
+
+
+
+NC:{
+
+name:"NC 다이노스",
+
+color:"#315288",
+
+money:150,
+
+power:88
+
+}
+
+
+};
